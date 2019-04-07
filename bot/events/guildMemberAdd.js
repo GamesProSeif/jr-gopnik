8d77530ce00b5c5d3045ca2f1a327a5ea754e837
GamesProSeif <seifmansour2014@outlook.com>
2019-04-07 22:24:36 +0300

    Added more events

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -8,7 +8,7 @@ const handler = async (bot, member) => {
   ];
   await member.addRole(bot.config.roles.kid);
 
-  let generalChanenl = member.guild.channels.find(c => c.name.toUpperCase() === 'GENERAL');
+  let generalChannel = member.guild.channels.find(c => c.name.toUpperCase() === 'GENERAL');
   let toBeSent = joinMessages[bot.functions.getRandom(0, joinMessages.length)];
   generalChannel.send(toBeSent);
 }
