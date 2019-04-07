e0c09f8b50f8420b7dc3d32ccadf8a09f8da2f50
GamesProSeif <seifmansour2014@outlook.com>
2019-04-07 23:19:41 +0300

    Added more join messages

--- bot/events/guildMemberAdd.js
+++ bot/events/guildMemberAdd.js
@@ -4,7 +4,12 @@ const handler = async (bot, member) => {
     `Thanks god, thought you were Quad for a second. Welcome, ${member}!`,
     `That's right, you are here with goats, ${member}.`,
     `Looks like ${member} came to babysit the goats...`,
-    `Did you just say baaa? Already becoming a goat so fast? Welcome, ${member}!`
+    `Did you just say baaa? Already becoming a goat so fast? Welcome, ${member}!`,
+    `${member} sacrifices to the goat lord`,
+    `Hope you brought food ${member}, goats will literally eat anything.`,
+    `What kind of username is this? Just come in ${member} smh ~~jk~~`,
+    `Wait, are you a sheep ${member}? ALL GOATS SCREAM!`,
+    `${member} joined. Hot goat identified. Breeding with ${member.guild.members.random()}`
   ];
   await member.addRole(bot.config.roles.kid);
 
