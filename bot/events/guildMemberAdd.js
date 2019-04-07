32fe10130fffd0a619fc40d4ee44a821a5ab6a2b
GamesProSeif <seifmansour2014@outlook.com>
2019-04-07 22:11:17 +0300

    Added more events

---
+++ bot/events/guildMemberAdd.js
@@ -0,0 +1,16 @@
+const handler = async (bot, member) => {
+  let joinMessages = [
+    `${member}, Welcome to the server, you mug!`,
+    `Thanks god, thought you were Quad for a second. Welcome, ${member}!`,
+    `That's right, you are here with goats, ${member}.`,
+    `Looks like ${member} came to babysit the goats...`,
+    `Did you just say baaa? Already becoming a goat so fast? Welcome, ${member}!`
+  ];
+  await member.addRole(bot.config.roles.kid);
+
+  let generalChanenl = member.guilds.channels.find(c => c.name.toUpperCase() === 'GENERAL');
+  let toBeSent = joinMessages[bot.functions.getRandom(0, joinMessages.length)];
+  generalChannel.send(toBeSent);
+}
+
+module.exports = handler;
---
+++ bot/events/guildMemberRemove.js
@@ -0,0 +1,13 @@
+const handler = (bot, member) => {
+  let leaveMessages = [
+    `Looks like someone's mad, ${member} bbye.`,
+    `What did I do to you ${member} ;-;. See ya forever ~~who knows~~`,
+    `You said you won't leave me again ${member} 😢`,
+    `Wa-wai-wait.. did ${member} just leave?`
+  ];
+  let generalChanenl = member.guilds.channels.find(c => c.name.toUpperCase() === 'GENERAL');
+  let toBeSent = leaveMessages[bot.functions.getRandom(0, leaveMessages.length)];
+  generalChannel.send(toBeSent);
+}
+
+module.exports = handler;
