070389b3839afac30b7463ca416a5a40cb63ff94
GamesProSeif <seifmansour2014@outlook.com>
2019-04-10 01:04:55 +0300

    fixed bug in messageDelete event

--- bot/events/messageDelete.js
+++ bot/events/messageDelete.js
@@ -2,7 +2,7 @@ const handler = (bot, message) => {
   if (message.author.bot || message.channel.type !== 'text') return;
 
   let name = message.channel.name.toUpperCase();
-  if (name == 'NSFW' || name == 'HOT-GUYS' || NAME == 'HOT-GIRLS') return;
+  if (name == 'NSFW' || name == 'HOT-GUYS' || name == 'HOT-GIRLS') return;
 
   let channelMessages = bot.deletedMessages.filter(m => m.channel.id === message.channel.id);
 
