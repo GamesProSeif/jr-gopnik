dd29e5ef4d23e6606d51b12359b086728383171b
GamesProSeif <seifmansour2014@outlook.com>
2019-04-10 01:02:29 +0300

    Changed functionality of snipe

--- bot/commands/snipe.js
+++ bot/commands/snipe.js
@@ -1,126 +1,94 @@
 const { RichEmbed } = require('discord.js');
 
-exports.run = (bot, message, args) => {
-  if (!args[0]) {
-    let name = message.channel.name.toUpperCase();
-    if (name == 'NSFW' || name == 'HOT-GUYS' || NAME == 'HOT-GIRLS') {
-      return message.channel.send('I dont\'t work for dirty people smh');
-    }
-
-    let channelMessages = bot.deletedMessages.filter(m => m.channel.id === message.channel.id);
-    if (channelMessages.size == 0) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: `No message found in ${message.channel.toString()}`,
-        color: bot.config.colors.error
-      }});
-    }
-    let deletedMessage = channelMessages.last();
-    let embed = new RichEmbed()
-      .setColor(bot.config.colors.info)
-      .setAuthor(deletedMessage.author.tag, deletedMessage.author.avatarURL)
-      .setTimestamp(deletedMessage.createdTimestamp);
-
-    if (deletedMessage.content) {
-      embed.setDescription(deletedMessage.content)
-    }
-    if (deletedMessage.attachments.first()) {
-      let files = deletedMessage.attachments.map(att => att.proxyURL)
-      embed.attachFiles(files);
-    }
-    message.channel.send(embed);
-  }
-  else if (message.mentions.channels.first() && args.length == 1) {
-    let name = message.mentions.channels.first().name.toUpperCase();
-    if (name == 'NSFW' || name == 'HOT-GUYS' || NAME == 'HOT-GIRLS') {
-      return message.channel.send('I dont\'t work for dirty people smh');
-    }
-    let channelId = message.mentions.channels.first().id;
-    let channelMessages = bot.deletedMessages.filter(m => m.channel.id === channelId);
-    if (channelMessages.size == 0) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: `No message found in ${message.mentions.channels.first().toString()}`,
-        color: bot.config.colors.error
-      }});
-    }
-    let deletedMessage = channelMessages.last();
-
-    let embed = new RichEmbed()
-      .setColor(bot.config.colors.info)
-      .setAuthor(deletedMessage.author.tag, deletedMessage.author.avatarURL)
-      .setTimestamp(deletedMessage.createdTimestamp);
-
-    if (deletedMessage.content) {
-      embed.setDescription(deletedMessage.content)
-    }
-    if (deletedMessage.attachments.first()) {
-      let files = deletedMessage.attachments.map(att => att.proxyURL)
-      embed.attachFiles(files);
-    }
-    message.channel.send(embed);
-  }
-  else if (!isNaN(args.join(' '))) {
-    let name = message.channel.name.toUpperCase();
-    if (name == 'NSFW' || name == 'HOT-GUYS' || NAME == 'HOT-GIRLS') {
-      return message.channel.send('I dont\'t work for dirty people smh');
-    }
-    let num = args.join(' ');
-    if (!Number.isInteger(parseFloat(num)) || parseInt(num) < 1) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: `Invalid argument: ${args.join(' ')}`,
-        color: bot.config.colors.error
-      }});
-    }
-    num = parseInt(num);
-    if (num > 10) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: 'Maximum message position is 10',
-        color: bot.config.colors.error
-      }});
-    }
-    let channelMessages = bot.deletedMessages.filter(m => m.channel.id === message.channel.id);
-    if (channelMessages.size == 0) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: `No message found in ${message.channel.toString()}`,
-        color: bot.config.colors.error
-      }});
-    }
-    if (num > channelMessages.size) {
-      return message.channel.send({embed:{
-        title: 'Error',
-        description: `Current number of cached deleted messages is ${channelMessages.size}`,
-        color: bot.config.colors.error
-      }});
-    }
-    let arr = channelMessages.array();
-    let deletedMessage = arr[channelMessages.size - num];
-    let embed = new RichEmbed()
-      .setColor(bot.config.colors.info)
-      .setAuthor(deletedMessage.author.tag, deletedMessage.author.avatarURL)
-      .setTimestamp(deletedMessage.createdTimestamp);
-
-    if (deletedMessage.content) {
-      embed.setDescription(deletedMessage.content)
-    }
-    if (deletedMessage.attachments.first()) {
-      let files = deletedMessage.attachments.map(att => att.proxyURL)
-      embed.attachFiles(files);
-    }
-    message.channel.send(embed);
-  }
-  else {
-    message.channel.send({embed:{
-      title: 'Error',
-      description: `Invalid argument: ${args.join(' ')}`,
-      color: bot.config.colors.error
-    }});
+const genError = (bot, args) => {
+  return {
+    title: 'Error',
+    description: `Invalid argument: ${args.join(' ')}`,
+    color: bot.config.colors.error
   }
 }
 
-exports.desc = 'Gets deleted message (message-position only works in the current channel)';
-exports.usage = '[ChannelMention | MessagePosition]'
+const getSniped = (bot, channel, position) => {
+  position = position ? position : 1;
+  let cname = channel.name.toUpperCase();
+  if (cname == 'NSFW' || cname == 'HOT-GUYS' || cname == 'HOT-GIRLS') {
+    return 'I dont\'t work for dirty people smh';
+  }
+  if (!Number.isInteger(parseFloat(position)) || parseInt(position) < 1) {
+    return {
+      title: 'Error',
+      description: `Invalid argument: ${args.join(' ')}`,
+      color: bot.config.colors.error
+    }
+  }
+  position = parseInt(position);
+  if (position > 10) {
+    return {
+      title: 'Error',
+      description: 'Maximum message position is 10',
+      color: bot.config.colors.error
+    }
+  }
+  let channelMessages = bot.deletedMessages.filter(m => m.channel.id === message.channel.id);
+  if (channelMessages.size == 0) {
+    return {
+      title: 'Error',
+      description: `No message found in ${channel}`,
+      color: bot.config.colors.error
+    }
+  }
+  if (position > channelMessages.size) {
+    return {
+      title: 'Error',
+      description: `Current number of cached deleted messages is ${channelMessages.size}`,
+      color: bot.config.colors.error
+    }
+  }
+  let arr = channelMessages.array();
+  let deletedMessage = arr[arr.length - position];
+
+  let embed = new RichEmbed()
+    .setColor(bot.config.colors.info)
+    .setAuthor(deletedMessage.author.tag, deletedMessage.author.avatarURL)
+    .setTimestamp(deletedMessage.createdTimestamp);
+
+  if (deletedMessage.content) {
+    embed.setDescription(deletedMessage.content);
+  }
+  if (deletedMessage.attachments.first()) {
+    let files = deletedMessage.attachments.map(att => att.proxyURL);
+    embed.attachFiles(files);
+  }
+
+  return embed;
+}
+
+exports.run = (bot, message, args) => {
+  if (!args) {
+    let embed = getSniped(bot, message.channel);
+    return message.channel.send(embed);
+  }
+  else if (message.mentions.channels.first() && args.length == 1) {
+    let channel = message.mentions.channels.first();
+    let embed = getSniped(bot, channel);
+    return message.channel.send(embed);
+  }
+  else if ((message.mentions.channels.first() && !isNaN(args.slice(1).join(' '))) {
+    let channel = message.mentions.channels.first();
+    let position = parseFloat(args[1]);
+    let embed = getSniped(bot, channel, position);
+    return message.channel.send(embed);
+  }
+  else if (!isNaN(args.join(' '))) {
+    let position = parseFloat(args[1]);
+    let embed = getSniped(bot, message.channel, position);
+    return message.channel.send(embed);
+  }
+  else {
+    return message.channel.send(genError(bot, args));
+  }
+}
+
+exports.desc = 'Gets deleted message';
+exports.usage = '[ChannelMention [position] | position]';
 exports.guildOnly = true;
