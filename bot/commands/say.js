2dc804a8c5bf1ad51bb9b09baa3edbaadedaad65
GamesProSeif <seifmansour2014@outlook.com>
2019-04-22 14:35:04 +0300

    Fixed bug in say command (also again)

--- bot/commands/say.js
+++ bot/commands/say.js
@@ -1,6 +1,6 @@
 exports.run = (bot, message, args) => {
   message.channel.send(args.join(' '));
-  if (message.channel.type == 'text') = message.delete();
+  if (message.channel.type == 'text') message.delete();
 }
 
 exports.desc = 'Makes the bot say a message';
