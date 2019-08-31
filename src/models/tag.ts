5331a54c68abbef118a8ed492fcaf75c54659a8c
GamesProSeif <seifmansour2014@outlook.com>
2019-08-31 21:05:46 +0300

    Initial tag commit

---
+++ src/models/tag.ts
@@ -0,0 +1,45 @@
+import { model, Schema } from 'mongoose';
+import { ITag } from '../typings';
+
+export const TagSchema = new Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  guild: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  hoisted: {
+    type: Boolean,
+    default: false
+  },
+  aliases: {
+    type: [String]
+  },
+  uses: {
+    type: Number,
+    default: 0
+  },
+  lastModified: {
+    type: String
+  },
+  createdAt: {
+    type: Number,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Number,
+    default: Date.now
+  }
+});
+
+export const TagModel = model<ITag>('guilds', TagSchema);
