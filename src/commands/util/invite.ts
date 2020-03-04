1a570dfc41b96c17a8f5a415a3c722c5273ebb28
GamesProSeif <seifmansour2014@outlook.com>
2020-03-05 00:46:31 +0300

    Update invite command

--- src/commands/util/invite.ts
+++ src/commands/util/invite.ts
@@ -1,12 +1,14 @@
+import { stripIndents } from 'common-tags';
 import { Command } from 'discord-akairo';
-import { Message } from 'discord.js';
+import { Message, MessageEmbed } from 'discord.js';
+import { COLORS } from '../../util/constants';
 
 export default class InviteCommand extends Command {
 	constructor() {
 		super('invite', {
 			aliases: ['invite', 'invite-bot'],
 			description: {
-				content: 'Generates an invite link for adding bot to servers'
+				content: 'Generates an invite link for adding bot to servers and the official server invite'
 			},
 			category: 'util'
 		});
@@ -15,6 +17,15 @@ export default class InviteCommand extends Command {
 	public async exec(message: Message) {
 		const link = await this.client.generateInvite(['SEND_MESSAGES']);
 
-		return message.util!.send(`<${link}>`);
+		const embed = new MessageEmbed()
+			.setColor(COLORS.PRIMARY)
+			.setTitle('❯ Invites')
+			.setDescription(stripIndents`
+			• Bot Invite: <${link}>
+			• Official Server <https://discord.gg/Jj5xmuK>
+			`)
+			.setFooter(this.client.user?.tag, this.client.user?.avatarURL()!);
+
+		return message.util!.send(embed);
 	}
 }
--- src/listeners/commandHandler/commandFinished.ts
+++ src/listeners/commandHandler/commandFinished.ts
@@ -12,9 +12,15 @@ export default class CommandFinishedListener extends Listener {
 	}
 
 	public exec(message: Message, command: Command, args: any) {
+		let argsStr = args;
+		try {
+			argsStr = JSON.stringify(args);
+		} catch {
+			argsStr = args.toString();
+		}
 		this.client.logger.info(
 			`Finished ${command.id} on ${message.guild ? `${message.guild.name} (${message.guild.id})` : 'DM'}${
-				Object.keys(args).length && !args.command ? ` with arguments ${JSON.stringify(args)}` : ''
+				Object.keys(args).length && !args.command ? ` with arguments ${argsStr}` : ''
 			}`,
 			{ topic: TOPICS.DISCORD_AKAIRO, event: EVENTS.COMMAND_FINISHED }
 		);
